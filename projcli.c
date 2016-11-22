78bedd40afa7e0067889e1dacc0af4e560b9d9b2
김혜원 <gpdnjs8151@naver.com>
2016-11-22 22:53:10 +0900

    카드 shuffle

--- projcli.c
+++ projcli.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main() {
-	char result[16] = {'a','a','b','b','c','d','d','e','e','f','f','g','g','h','h'};
+	char result[16] = {'a','a','b','b','c','c','d','d','e','e','f','f','g','g','h','h'};
+	char temp;
+	int i;
+	int randNumber;
+
+	srand((unsigned)time(NULL));
+	for(i=0; i<16; i++) {
+		randNumber = rand()%16;
+		temp = result[i];
+		result[i] = result[randNumber];
+		result[randNumber] = temp;
+	}
+
+	for (int i = 0; i < 16; ++i)
+	{
+		printf("%c",result[i]);
+		if ((i+1)%4 == 0)
+		{
+			printf("\n");
+		}
+	}
 }
