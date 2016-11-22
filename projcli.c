d84103d3c7be0805701976a4cafc8671ff3e43b9
김혜원 <gpdnjs8151@naver.com>
2016-11-22 23:24:30 +0900

    같은 자리에 print

--- projcli.c
+++ projcli.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
+
+void gotoxy(int x,int y);
+
 
 int main() {
 	char result[16] = {'a','a','b','b','c','c','d','d','e','e','f','f','g','g','h','h'};
 	char temp;
-	int i;
+	int i,j=3,k=3;
 	int randNumber;
+	char mesg[]="Just a string";		/* message to be appeared on the screen */
 
 	srand((unsigned)time(NULL));
+
 	for(i=0; i<16; i++) {
 		randNumber = rand()%16;
 		temp = result[i];
@@ -18,10 +24,26 @@ int main() {
 
 	for (int i = 0; i < 16; ++i)
 	{
+		gotoxy(j,k); //reposition cursor
 		printf("%c",result[i]);
+		j=j+2;
 		if ((i+1)%4 == 0)
 		{
 			printf("\n");
+			k = k+2;
+			j=3;
 		}
 	}
+
+
 }
+
+//gotoxy function
+void gotoxy(int x,int y)
+{
+printf("%c[%d;%df",0x1B,y,x);
+}
+// int main() {
+// gotoxy(25,2); //reposition cursor
+// printf("hello world"); //display text
+// }
